0285005d56c12b157dc3f62a384e403ab444e764
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-06-15 21:58:26 +0200

    Update Program.cs

--- FileManagement/FileManagementSimulation/FileManagementSimulation/Program.cs
+++ FileManagement/FileManagementSimulation/FileManagementSimulation/Program.cs
@@ -7,16 +7,22 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             //Experiment 1
-            var keys = File.ReadAllText(@"D:\OSCO\FileManagement\TransactionFile.txt").Split(
-             new string[] { Environment.NewLine },
-                 StringSplitOptions.None);
+            //var keys = File.ReadAllText(@"D:\OSCO\FileManagement\TransactionFile.txt").Split(
+            //new string[] { Environment.NewLine },
+            //StringSplitOptions.None);
 
-            var pileFile = File.ReadAllText(@"D:\OSCO\FileManagement\PileFile.txt").Split(
-             new string[] { Environment.NewLine },
-                 StringSplitOptions.None);
+            //var pileFile = File.ReadAllText(@"D:\OSCO\FileManagement\PileFile.txt").Split(
+            //new string[] { Environment.NewLine },
+            //StringSplitOptions.None);
 
-
-
+            var path = "D:\\OSCO\\FileManagement\\TransactionFile.txt";
+            var bytesLength = File.ReadAllBytes(path).Length;
+            Console.WriteLine($"count the number of byte accesses needed {bytesLength}");
+            var averageAccessTime = File.ReadAllBytes(path);
+            var current = File.GetLastAccessTime(path).Second;
+            Console.WriteLine(current);
+            File.ReadLines(@"D:\OSCO\FileManagement\TransactionFile.txt").SkipWhile(line => !line.Contains("CustomerEN"))
+                .TakeWhile(line => !line.Contains('\n'));
         }
     }
 }
